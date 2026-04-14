9b53e8999556a084c96ccf4750b9732aa71c5817
Joseph-Mamian <jsmamianb@ufpso.edu.co>
2026-04-13 19:24:52 -0500

    clase lunes

---
+++ android/app/src/main/kotlin/com/example/dispm3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dispm3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/api.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+
+class Api {
+
+  static final _dio = Dio();
+
+  static Future<String> getToken (String email, String pass) async{
+    try{
+    final response = await _dio.post(
+  
+    "https://api.escuelajs.co/api/v1/auth/login",
+    data: {
+      "email": email,
+      "password": pass
+
+    });
+
+    if(response.statusCode != 201){
+     return "Error en el login";
+    }
+
+    return 'city boy city boy';
+    } on DioException catch(e){
+      return 'Error';
+    }
+    
+    
+  }
+
+
+}
